ebe3f421c97215315d5ad82386574c3206d463ae
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2024-06-05 11:19:39 +0100

    renamed ll<> to ll-t (linkedlist with template). Fixes #1

--- C++/linkedlist/ll-t.cpp
+++ C++/linkedlist/ll-t.cpp
@@ -174,3 +174,27 @@ public:
 };
 
 #endif
+
+int main() {
+  Linkl<int> obj;
+  obj.insertFirst(10);
+  obj.insertFirst(20);
+  obj.insertFirst(3);
+  obj.insertFirst(40);
+  obj.insertFirst(50);
+  obj.insertFirst(60);
+  obj.insertLast(96);
+  obj.display();
+  cout << "Initial First Data: " << obj.popFirst().data;
+  cout << " Initial Last Data: " << obj.popLast().data << endl;
+  obj.display();
+  obj.insertAt(4, 123);
+  obj.display();
+  obj.length();
+  // Linkl newobj;
+  // obj.reverse(newobj);
+  // newobj.display();
+  obj.reverseInPlace();
+  obj.display();
+  return 0;
+}

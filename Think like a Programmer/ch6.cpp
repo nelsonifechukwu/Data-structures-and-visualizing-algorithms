a78dfb499e376d0e4f68fe171204292285d9f334
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2025-05-19 01:10:36 +0100

    understanding recursion

---
+++ Think like a Programmer/ch6.cpp
@@ -0,0 +1,44 @@
+// head vs tail recursion
+// in head, the minimum data is passed through the function calls. In tail, it's
+// the max
+
+// in recursion the calling functions only care about 'what' from the functions
+// they call. they don't care how the functions they call reaches a conclusion.
+
+// consequently, all recursive functions can then be distilled into a
+// dispatcher.
+
+/*
+a dispatcher:
+1) handles the base (most trivial) case
+2) must pass a smaller version of the problem to the next call.
+
+The big idea of recursion is to move from dispatching to a recursive calls.
+*/
+
+
+int iterativeArraySum(int integers[], int size) {
+  int sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += integers[i];
+  }
+  return sum;
+}
+
+//dispatcher in action
+int arraySumDelegate(int integers[], int size) {
+  if (size == 0)
+    return 0;
+  int lastNumber = integers[size - 1];
+  int allButLastSum = iterativeArraySum(integers, size - 1);
+  return lastNumber + allButLastSum;
+}
+
+//dispatcher replaced by a recursive call
+int arraySumRecursive(int integers[], int size) {
+  if (size == 0)
+    return 0;
+  int lastNumber = integers[size - 1];
+  int allButLastSum = arraySumRecursive(integers, size - 1);
+  return lastNumber + allButLastSum;
+}
